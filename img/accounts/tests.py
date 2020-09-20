64f01e95cb9c4719cc85ed10af1a735bc3300f32
Tushar912 <tushar.912u@gmail.com>
2020-09-20 19:55:45 +0530

    added test for login

--- img/accounts/tests.py
+++ img/accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+User = get_user_model()
+
+class LoginTestCase:
+    
+    def test_login_redirect_if_logged_in(self):
+        response = self.client.get('chat:home')
+        self.assertRedirects(response,'/accounts/login/?next=/')
+
+        with self.settings(LOGIN_URL='/accounts/register/'):
+            response = self.client.get('')
+            self.assertRedirects(response, '/accounts/register/?next=/')
+
+
+
