667bf22806cf799c489656c8bdc2086b3afe5448
Tushar912 <tushar.912u@gmail.com>
2020-09-20 20:17:01 +0530

    added test for register

--- img/accounts/tests.py
+++ img/accounts/tests.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 User = get_user_model()
 
-class LoginTestCase:
+class LoginTestCase(TestCase):
     
     def test_login_redirect_if_logged_in(self):
         response = self.client.get('chat:home')
@@ -19,3 +19,67 @@ class LoginTestCase:
     def test_login_template:
         response = self.client.get(reverse('accounts:login'))
         self.assertTemplateUsed(response, 'registration/login.html')
+
+
+class RegisterationTestCase(TestCase):
+
+    def test_redirect_if_user_logged_in(self):
+        _user = User.objects.create(username='abc', email='abc@test.com')
+        _user.set_password('abc')
+        _user.save()
+        self.client.login(username='abc', password='abc')
+        response = self.client.get(reverse('accounts:register'))
+        self.assertRedirects(response, reverse('chat:home'))
+
+    def test_valid_registration(self):
+        valid_reg_data = {
+            'username': 'john',
+            'email': 'john@test.com',
+            'first_name': 'john',
+            'last_name': 'doe',
+            'password1': '@secret123',
+            'password2': '@secret123'
+        }
+        response = self.client.post(reverse('accounts:register'), valid_reg_data)
+        self.assertRedirects(response, reverse('chat:home'))
+        self.assertEqual(User.objects.count(), 1)        
+
+    
+    def test_invalid_registration(self):
+        invalid_reg_data = [
+            {
+                
+                'username': 'john',
+                'email': 'john@test.com',
+                'first_name': 'john',
+                'last_name': 'doe',
+                'password1': 'secret12',
+                'password2': 'secret123'
+            },
+            {
+                
+                'username': 'john',
+                'email': 'johntest.com',
+                'first_name': 'john',
+                'last_name': 'doe',
+                'password1': 'secret123',
+                'password2': 'secret123'
+            },
+            {
+                
+                'username': '!john',
+                'email': 'john@test.com',
+                'first_name': 'john',
+                'last_name': 'doe',
+                'password1': 'secret12',
+                'password2': 'secret123'
+            },
+
+        ]
+        for invalid_form in invalid_reg_data:
+            response = self.client.post(reverse('accounts:register'), invalid_form)
+            self.assertEqual(response.status_code, 200)
+
+    def test_register_page_templates(self):
+        response = self.client.get(reverse('accounts:register'))
+        self.assertTemplateUsed(response, 'registration/register.html')
