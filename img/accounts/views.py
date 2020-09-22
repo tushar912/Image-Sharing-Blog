c3b05b6807342f82aa8b15718db5a320278c6341
Tushar912 <tushar.912u@gmail.com>
2020-09-22 18:32:53 +0530

    added register_view

--- img/accounts/views.py
+++ img/accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, get_user_model, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-# Create your views here.
+from .forms import ProfileForm, RegistrationForm
+from .models import UserProfile
+
+User = get_user_model()
+
+def register(request):
+    
+    if request.user.is_authenticated:
+        return redirect(reverse('chat:home'))
+
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        # Process POSTed Form data
+        if form.is_valid():
+            new_user = form.save()
+            new_user = authenticate(username=request.POST['username'],
+                                    password=request.POST['password1']
+                                    )
+            # authenticate and log user in, then redirect to newsFeeds
+            login(request, new_user)
+            return redirect(reverse('chat:home'))
+
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
