ae0c7ef6297eda241d1a46bfafcc1faef0550525
Tushar912 <tushar.912u@gmail.com>
2020-09-22 19:03:51 +0530

    added custum user

--- img/accounts/admin.py
+++ img/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, UserProfile
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = (UserProfileInline, )
+
+admin.site.register(User, UserAdmin)
--- img/accounts/urls.py
+++ img/accounts/urls.py
@@ -6,9 +6,9 @@ app_name = 'accounts'
 urlpatterns = [
 	path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
-    path('followers/', views.followers, name='followers'),
-    path('my-profile/edit/', views.edit_profile, name='edit_profile'),
-    path('profile/<username>/', views.profile, name='view-profile'),
-    path('users/follow/<username>/', views.follow, name='follow'),
-    path('users/unfollow/<username>/', views.unfollow, name='unfollow'),
+    # path('followers/', views.followers, name='followers'),
+    # path('my-profile/edit/', views.edit_profile, name='edit_profile'),
+    # path('profile/<username>/', views.profile, name='view-profile'),
+    # path('users/follow/<username>/', views.follow, name='follow'),
+    # path('users/unfollow/<username>/', views.unfollow, name='unfollow'),
 ]
