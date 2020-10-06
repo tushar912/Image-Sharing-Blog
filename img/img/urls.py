5fcb1d36efee9fd6c4c2512d82f1c2769ed8c427
Tushar912 <tushar.912u@gmail.com>
2020-10-06 19:33:20 +0530

    add chat url

--- img/img/urls.py
+++ img/img/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
+    path('', include('chat.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
