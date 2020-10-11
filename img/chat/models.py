991ea15c9550349368c9d182d57749532e9a1b55
Tushar912 <tushar.912u@gmail.com>
2020-10-11 15:32:49 +0530

    added chat model

--- img/chat/models.py
+++ img/chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+	
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts', on_delete=models.CASCADE)
+	picture = models.ImageField(upload_to='posts', blank=True)
+	text = models.TextField(max_length=2048, blank=True)
+	posted_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{self.user.username}'s post"
+
+class Comment(models.Model):
+	
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments', on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+	text = models.TextField(max_length=2048, blank=True)
+	comment_date = models.DateTimeField(auto_now_add=True)
