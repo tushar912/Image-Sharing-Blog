fb24466a271a0d86d107410ccbd86305f4cdeedf
Tushar912 <tushar.912u@gmail.com>
2020-10-11 15:53:18 +0530

    add urls

---
+++ img/chat/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'chat'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('posts/add', views.add_post, name='add_post'),
+    path('comments/add/<post_id>', views.add_comment, name='add_comment')
+]
