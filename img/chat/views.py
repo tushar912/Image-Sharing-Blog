a26ae58b85616adbbd179279fd074fd9e3b1d8ac
Tushar912 <tushar.912u@gmail.com>
2020-10-11 17:22:42 +0530

    add home view

--- img/chat/views.py
+++ img/chat/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.views.decorators.http import require_POST
+
+from .forms import CommentForm, PostForm
+from .models import Post
 
 
+@login_required
+def home(request):
+	""" The home news feed page """
+
+	# Get users whose posts to display on news feed and add users account
+	_users = list(request.user.followers.all())
+	_users.append(request.user)
+
+	# Get posts from users accounts whose posts to display and order by latest
+	posts = Post.objects.filter(user__in=_users).order_by('-posted_date')
+	comment_form = CommentForm()
+	return render(request, 'chat/home.html', {'posts': posts, 'comment_form': comment_form})
